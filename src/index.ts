940292f011981f138742c021f5d7f243df95894b
CosmoArt <cosmohydra17@gmail.com>
2022-07-12 20:26:29 -0500

    js optimization

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import "./style.scss";
 const $share = document.getElementById("share"),
     $shareMenu = document.querySelector(".shareMenu");
 
-window.matchMedia("(min-width: 768px)").matches ? $share!.addEventListener("click", () => $shareMenu!.classList.toggle("active2")) : $share!.addEventListener("click", () => $shareMenu!.classList.toggle("active"));
+$share!.addEventListener("click", () => $shareMenu!.classList.toggle("active"));
